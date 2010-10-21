3800a96f557edc0f4b1765f4b467a45cd05f39d8
nikodem <nikodem@theochem.tu-muenchen.de>
2010-10-21 09:48:46 +0000

    dlb: improve debugging output main

--- main.f90
+++ main.f90
@@ -10,19 +10,23 @@ use dlb
 use test2, only: test_calc
 implicit none
 include 'mpif.h'
-integer :: rank, n_procs
+integer :: rank, n_procs, my_amount
 integer :: ierr, prov
 integer :: i, res
 integer :: my_jobs(2)
 integer, pointer :: sec(:)
 
 integer, parameter :: num_jobs = 20
-integer, parameter :: n = 3
+logical, parameter :: equal =  .false.
+integer, parameter :: n = 1
 integer, target    :: my_parts(n)
 double precision :: sum_all(2,1)
 
 double precision :: time
-double precision, allocatable :: times(:, :)
+double precision, allocatable :: times(:, :), times2(:,:), loops(:)
+integer, allocatable :: jobs_done(:,:)
+
+!call sleep(30)
 
 call MPI_INIT_THREAD(MPI_THREAD_MULTIPLE, prov, ierr)
 sum_all = 0
@@ -41,15 +45,27 @@ end select
 
 call MPI_COMM_RANK( MPI_COMM_WORLD, rank, ierr )
 call MPI_COMM_SIZE( MPI_COMM_WORLD, n_procs, ierr)
-print *, "Hello World!, I'm", rank, "of", n_procs
+if ( rank == 0 ) then
+  print *, "=============================================================================="
+  if (equal) then
+     print *, "Run with", num_jobs, "jobs, equal distributed"
+  else
+     print *, "Run with", num_jobs, "jobs, worst case"
+  endif
+endif
+!print *, "Hello World!, I'm", rank, "of", n_procs
 
 !
 ! times(:, 1) --- elapsed
 ! times(:, 2) --- working (usful work)
 !
 allocate(times(0:n_procs-1, 2))
+allocate(times2(0:n_procs-1, 5))
+allocate(loops(0:n_procs-1))
+allocate(jobs_done(0:n_procs-1,2))
 times = 0.0
-
+times2 = 0.0
+jobs_done = 0
 !
 ! Place all jobs on highest rank for testing:
 !
@@ -60,49 +76,83 @@ else
   my_jobs(1) = 0
   my_jobs(2) = 0
 endif
+if (equal) then
+  my_amount = num_jobs / n_procs
+  my_jobs(1) = (rank) * my_amount
+  my_jobs(2) = my_jobs(1) + my_amount
+  if (rank+1 == n_procs) my_jobs(2) = num_jobs
+endif
 print *, rank, ":My jobs:", my_jobs
-
+jobs_done(rank, 1) = my_jobs(2) - my_jobs(1)
 call time_stamp("START",0)
 
 ! starting time:
 times(rank, 1) = MPI_WTIME()
+times2(rank,1) = MPI_WTIME()
 
 call dlb_init()
 
+times2(rank,2) = MPI_WTIME()
 call dlb_setup(my_jobs)
+times2(rank,3) = MPI_WTIME()
 do while (more_work(n, sec))
    ! measure working time:
    time = MPI_WTIME()
 
-   call time_stamp("START USEFUL WORK",0)
+   !call time_stamp("START USEFUL WORK",0)
 
    do i = 1, size(sec)
      res = test_calc(sec(i))
      sum_all(1,:) = sum_all(1,:) + res
      sum_all(2,:) = sum_all(2,:) + sec(i)
+     jobs_done(rank, 2) = jobs_done(rank, 2) + 1
    enddo
 
-   call time_stamp("STOP USEFUL WORK",0)
+   !call time_stamp("STOP USEFUL WORK",0)
 
    ! increment working time by this slice:
    times(rank, 2) = times(rank, 2) + (MPI_WTIME() - time)
 enddo
 
+times2(rank,4) = MPI_WTIME()
 call dlb_finalize()
+times2(rank,5) = MPI_WTIME()
 
 call time_stamp("END",0)
 ! elapsed time:
 times(rank, 1) = MPI_WTIME() - times(rank, 1)
 
 call reduce(times)
+call reduce(times2)
 call reduce(sum_all)
+call reduceint(jobs_done)
 
 if ( rank == 0 ) then
   print *,'elapsed times = ', times(:, 1)
   print *,'working times = ', times(:, 2)
   print *,'efficiency =', sum(times(:, 2)) / maxval(times(:, 1)) / size(times, 1)
+  print *, 'maximum elapsed times = ', maxval(times(:,1))
+  print *, 'maximum difference in working times =', maxval(times(:,2)) - minval(times(:,2))
   print *, 'result of calculation =', sum_all(1,:)
   print *, 'sum of job numbers = ', sum_all(2,:)
+  print *, 'Timings: Time points:'
+  print *, times2(:,1)
+  print *, times2(:,2)
+  print *, times2(:,3)
+  print *, times2(:,4)
+  print *, times2(:,5)
+  print *, 'Timings: durations:'
+  print *, "dlb_init", times2(:,2) - times2(:,1)
+  print *, "dlb_setup", times2(:,3) - times2(:,2)
+  loops = times2(:,4) - times2(:,3)
+  print *, "loops", loops(:)
+  print *, 'maximum difference in loops =', maxval(loops(:)) - minval(loops(:))
+  print *, "dlb_finalize", times2(:,5) - times2(:,4)
+  print *, "start jobs: ", jobs_done(:,1)
+  print *, "jobs done on the processors: ", jobs_done(:,2)
+  print *, "max/min jobs on procs: ", maxval(jobs_done(:,2)), minval(jobs_done(:,2))
+  print *, "away or get", jobs_done(:,2) - jobs_done(:,1)
+  print *, "away or get max/min", maxval(jobs_done(:,2) - jobs_done(:,1)), minval(jobs_done(:,2) - jobs_done(:,1))
 endif
 
 call MPI_FINALIZE(ierr)
@@ -172,5 +222,23 @@ contains
     endif
     if ( ierr /= MPI_SUCCESS ) stop "error in MPI_REDUCE"
   end subroutine reduce
+
+  subroutine reduceint(array)
+    implicit none
+    integer, intent(inout) :: array(:, :)
+    ! *** end of interface ***
+
+    integer :: rank, ierr
+
+    call MPI_COMM_RANK( MPI_COMM_WORLD, rank, ierr )
+    if ( ierr /= MPI_SUCCESS ) stop "error in MPI_COMM_RANK"
+
+    if ( rank == 0 ) then
+      call MPI_REDUCE( MPI_IN_PLACE, array, size(array), MPI_INTEGER4, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+    else
+      call MPI_REDUCE( array, MPI_IN_PLACE, size(array), MPI_INTEGER4, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+    endif
+    if ( ierr /= MPI_SUCCESS ) stop "error in MPI_REDUCE"
+  end subroutine reduceint
 
 end program main
