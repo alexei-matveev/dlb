03c20aa31329d5d8757877a3740a6b50ba1b89b9
matveev <matveev@theochem.tu-muenchen.de>
2010-11-09 17:55:13 +0000

    AM: test.f90 provides echo(i) that burns CPU and returns i

--- test.f90
+++ test.f90
@@ -2,38 +2,34 @@ module test
 
   implicit none
   private
-  public :: test_calc
 
-  integer :: i = 8
-  integer, parameter :: LOOP = 7
+  public :: echo
+
+  integer, private, save :: global_state
 
   contains
 
-  integer function test_calc(num)
+  integer function echo(i)
+    implicit none
+    integer, intent(in) :: i
+    ! *** end of interface ***
+
+    call burn_cpu(i)
+
+    !
+    ! Echo input:
+    !
+    echo = i
+  end function echo
+
+  subroutine burn_cpu(num)
     implicit none
     integer, intent(in) :: num
     ! *** end of interface ***
 
-    test_calc = test_calc2(num)
-  end function test_calc
+    integer :: i = 8
+    integer, parameter :: LOOP = 7
 
-  function test_calc1(num)
-    implicit none
-    integer, intent(in) :: num
-    integer :: test_calc1
-    ! *** end of interface ***
-
-    call sleep(1)
-    !if (mod(num,4) == 0) call sleep(3)
-    test_calc1 = num *2 - 90 + i ** 2 + num ** 3 /7
-  end function test_calc1
-
-  function test_calc2(num)
-    implicit none
-    integer, intent(in) :: num
-    ! *** end of interface ***
-
-    integer :: test_calc2
     integer :: j, l , l1, l2, l3, l4, l5, l6, l7, l8, l9
     real :: k
 
@@ -86,6 +82,7 @@ module test
     ! make result dependent on k, otherwise all loops above
     ! are optimized out:
     !
-    test_calc2 = num *2 - 90 + i ** 2 + num ** 3 /7 + k
-  end function test_calc2
+    global_state = num *2 - 90 + i ** 2 + num ** 3 /7 + k
+  end subroutine burn_cpu
+
 end module test
