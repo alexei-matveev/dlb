f85db9dadc1f0c900d15a77257f78fb8ed4045fd
matveev <matveev@theochem.tu-muenchen.de>
2010-11-09 18:35:30 +0000

    AM: (pseudo) randomize CPU time burned in burn_cpu(i) depending on i

--- test.f90
+++ test.f90
@@ -5,7 +5,7 @@ module test
 
   public :: echo
 
-  integer, private, save :: global_state
+  real, private, save :: global_state
 
   contains
 
@@ -22,19 +22,33 @@ module test
     echo = i
   end function echo
 
-  subroutine burn_cpu(num)
+  subroutine burn_cpu(i)
     implicit none
-    integer, intent(in) :: num
+    integer, intent(in) :: i
     ! *** end of interface ***
 
-    integer :: i = 8
     integer, parameter :: LOOP = 7
+    integer, parameter :: IMIN = LOOP - 3
+    integer, parameter :: IMAX = LOOP + 3
+    integer(8) :: i8 ! long int
+
+    integer :: random_cost
 
     integer :: j, l , l1, l2, l3, l4, l5, l6, l7, l8, l9
     real :: k
 
+    !
+    ! A pseudo random number between IMIN and IMAX:
+    !
+    i8 = i ! conversion to long int
+    random_cost = IMIN + mod(prand(prand(prand(i8))), IMAX - IMIN + 1)
+    ! print *, "random_cost(", i,") =", random_cost, "between", IMIN, "and", IMAX
+
     k = 0
-    do j = 1, LOOP
+    !
+    ! Randomize burned CPU time:
+    !
+    do j = 1, random_cost
       k = real(j)
       k = k + sqrt(k)
     do l = 1, LOOP
@@ -82,7 +96,19 @@ module test
     ! make result dependent on k, otherwise all loops above
     ! are optimized out:
     !
-    global_state = num *2 - 90 + i ** 2 + num ** 3 /7 + k
+    global_state = k
   end subroutine burn_cpu
+
+  function prand(i) result(p)
+    implicit none
+    integer(8), intent(in) :: i
+    integer(8)             :: p
+    ! *** end of interface ***
+
+    integer(8), parameter :: A = 134775813, B = 1
+    integer(8), parameter :: RANGE = 2_8**32
+
+    p = mod(A * i + B, RANGE)
+  end function prand
 
 end module test
