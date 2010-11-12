0fd52376a337dff0e4a7b626394feade6d4307f7
matveev <matveev@theochem.tu-muenchen.de>
2010-11-12 14:21:34 +0000

    AM dlb: rm unused and wrong macro assert_n(expr, code) and c-style comment

--- dlb.h
+++ dlb.h
@@ -8,9 +8,6 @@
 
 # define ASSERT(expr) if(.not.(expr))call DLB_ASSERT_FAILED(__STRING(expr),__FILE__,__LINE__)
 
-/* FIXME: must die, use ASSERT(expr) instead: */
-# define assert_n(expr, code) dlb_assert_failed(__STRING(expr),__FILE__,__LINE__)
-
 #ifdef USE_MPI_MODULE
 # define USE_MPI use mpi /* some build systems provide MPI module */
 #else
