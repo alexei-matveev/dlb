0f6f4db8d2e4d824b7031ef24ebc5892c1d580fb
matveev <matveev@theochem.tu-muenchen.de>
2012-09-19 09:32:14 +0000

    AM: format comments

--- dlb_impl_static.f90
+++ dlb_impl_static.f90
@@ -5,23 +5,30 @@ module dlb_impl
 
 !---------------------------------------------------------------
 !
-!  Purpose: takes care about dynamical load balancing, is the static
-!           routine, meaning, not dynamical, but for comparision with them
-!           with exactly the same interface
-!           INTERFACE to others:
-!           call dlb_init() - once before first acces
-!           call dlb_setup(init_job) - once every time a dlb should
-!                               be used, init_job should be the part
-!                               of the current proc of an initial job
-!                               distribution
-!           dlb_give_more(n, jobs) :
-!              n should be the number of jobs requested at once, the next
-!              time dlb_give_more is called again, all jobs from jobs should
-!              be finished, jobs are at most n, they are jsut the number of the
-!              jobs, which still have to be transformed between each other,
-!              it should be done the error slice from jobs(0) +1 to jobs(1)
-!              if dlb_give_more returns jobs(0)==jobs(1) there are on no proc
-!              any jobs left
+!  Takes care  about dynamical load balancing. Is  the static routine,
+!  meaning, not dynamical, but  for comparision with them with exactly
+!  the same interface.
+!
+!  INTERFACE to others:
+!
+!  call dlb_init()
+!
+!  Called once before first acces.
+!
+!  call dlb_setup(init_job)
+!
+!  Called once every time a dlb should be used, init_job should be the
+!  part of the current proc of an initial job distribution.
+!
+!  dlb_give_more(n, jobs)
+!
+!  n should  be the number  of jobs requested  at once. The  next time
+!  dlb_give_more()  is called  again,  all jobs  from  jobs should  be
+!  finished. Jobs are at most n, they are just the number of the jobs,
+!  which still have to be transformed between each other. It should be
+!  done the error slice from jobs(0) + 1 to jobs(1) if dlb_give_more()
+!  returns jobs(0) == jobs(1) there are on no proc any jobs left
+!
 !  Module called by: ...
 !
 !
@@ -64,8 +71,7 @@ integer(i4_kind) :: job_storage(JLENGTH) ! store all the jobs, belonging to this
 contains
 
   subroutine dlb_init(world)
-    !  Purpose: initalization of needed stuff
-    !------------ Modules used ------------------- ---------------
+    ! Initalization of needed stuff
     use dlb_common, only: set_empty_job, dlb_common_init
     use dlb_common, only: my_rank, time_stamp
     implicit none
@@ -83,8 +89,7 @@ contains
   end subroutine dlb_init
 
   subroutine dlb_finalize()
-    !  Purpose: cleaning up everything, after last call
-    !------------ Modules used ------------------- ---------------
+    ! Cleaning up everything, after last call
     use dlb_common, only: dlb_common_finalize
     implicit none
     ! *** end of interface ***
@@ -93,17 +98,18 @@ contains
   end subroutine dlb_finalize
 
   subroutine dlb_give_more(n, my_job)
-    !  Purpose: Returns next bunch of up to n jobs, if my_job(1)<=
-    !  my_job(2) there are no more jobs there, else returns the jobs
-    !  done by the procs should be my_job(1) + 1 to my_job(2) in
-    !  the related job list
-    !------------ Modules used ------------------- ---------------
+    !
+    ! Returns next  bunch of up to  n jobs, if  my_job(1) <= my_job(2)
+    ! there are no more jobs there,  else returns the jobs done by the
+    ! procs should  be my_job(1) + 1  to my_job(2) in  the related job
+    ! list
+    !
     use dlb_common, only: steal_local, JLEFT, JRIGHT
     use dlb_common, only: empty, time_stamp
     implicit none
-    !------------ Declaration of formal parameters ---------------
     integer(i4_kind), intent(in)  :: n
     integer(i4_kind), intent(out) :: my_job(:)
+
     !** End of interface *****************************************
     double precision              :: start_timer_gm
     integer(i4_kind) :: jobs(JLENGTH), remaining(JLENGTH)
@@ -137,18 +143,17 @@ contains
   end subroutine dlb_give_more
 
   subroutine dlb_setup(job)
-    !  Purpose: initialization of a dlb run, each proc should call
-    !           it with inital jobs. The inital jobs should be a
-    !           static distribution of all available jobs, each job
-    !           should be given to one and only one of the procs
-    !           jobs should be given as a job range (STP, EP), where
-    !           all jobs should be the numbers from START to END, with
-    !           START <= STP <= EP <= END
-    !------------ Modules used ------------------- ---------------
+    !
+    ! Initialization  of a  DLB run,  each  proc should  call it  with
+    ! inital jobs. The inital jobs  should be a static distribution of
+    ! all available jobs, each job should be given to one and only one
+    ! of the  procs jobs  should be  given as a  job range  (STP, EP),
+    ! where all  jobs should  be the numbers  from START to  END, with
+    ! START <= STP <= EP <= END
+    !
     use dlb_common, only: JLEFT, JRIGHT, JOWNER, my_rank
     use dlb_common, only: time_stamp
     implicit none
-    !------------ Declaration of formal parameters ---------------
     integer(i4_kind), intent(in) :: job(:)
     !** End of interface *****************************************
 
